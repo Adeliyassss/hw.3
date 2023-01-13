dd4bd99083ae3219c67fc1003832921ef2c8349f
Adeliyassss <100111844+Adeliyassss@users.noreply.github.com>
2023-01-13 17:35:16 +0600

    Initial commit

---
+++ src/com/company/Main.java
@@ -0,0 +1,28 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        BankAccount user = new BankAccount();
+        user.deposit(20000);
+
+        while (true) {
+            try {
+                user.withDraw(6000);
+                System.out.println("Успешно снято");
+            } catch (LimitException e) {
+                System.out.println(e.getMessage());
+                try {
+                    user.withDraw((int) e.getRemainingAmount());
+                } catch (LimitException ex) {
+                    System.out.println(ex.getMessage());
+                }
+                break;
+
+            }
+
+
+        }
+
+    }
+}
